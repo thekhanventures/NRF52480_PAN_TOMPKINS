3c210c83bb2d844ae0a2d252a88e28e44e8eec1e
thekhanventures <126792014+thekhanventures@users.noreply.github.com>
2024-04-03 21:55:15 +0800

    Add files via upload

---
+++ src/panTompkins.h
@@ -0,0 +1,12 @@
+#ifndef PAN_TOMPKINS
+#define PAN_TOMPKINS
+#include <stdbool.h>
+
+typedef int dataType;
+/*typedef enum {false, true} bool;*/
+typedef enum { false_value = false, true_value = true } Boolean;
+
+void panTompkins();
+
+
+#endif
